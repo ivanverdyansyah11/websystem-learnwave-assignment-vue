ae72095eaccdb9c7ef3f78219eb6b12092da80c5
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-04 10:47:50 +0800

    Progress: Finish Component Hamburger

---
+++ src/components/Hamburger.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="hamburger d-flex d-lg-none justify-content-center align-items-center">
+      <img src="/src/assets/images/sidebar/hamburger.png" alt="Hamburger Icon" width="20" height="20">
+  </div>
+</template>
+
+<style scoped>
+</style>
